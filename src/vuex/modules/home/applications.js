4cfb8053f1591c13ced47ba95e5bcb1bda54d211
yan.zhang <yan.zhang@yunshipei.com>
2017-05-31 10:39:35 +0800

    1.0 封版

--- src/vuex/modules/home/applications.js
+++ src/vuex/modules/home/applications.js
@@ -13,14 +13,14 @@ export const SET_APPLIST           = 'applications/SET_APPLIST';
 const state  = {
 	// 更多应用列表
 	appList : [
-		{'name' : '销售任务', 'img' : './static/img/work/销售任务.png', 'show' : false, 'subApp' : [{'name' : '各种业务1'}, {'name' : '各种业务2'}, {'name' : '各种业务3'}]},
-		{'name' : '拜访管理', 'img' : './static/img/work/拜访管理.png', 'show' : false, 'subApp' : [{'name' : '各种业务1'}, {'name' : '各种业务2'}, {'name' : '各种业务3'}]},
-		{'name' : '信息协同', 'img' : './static/img/work/信息协同.png', 'show' : false, 'subApp' : [{'name' : '各种业务1'}, {'name' : '各种业务2'}, {'name' : '各种业务3'}]},
-		{'name' : '数据看板', 'img' : './static/img/work/数据看板.png', 'show' : false, 'subApp' : [{'name' : '各种业务1'}, {'name' : '各种业务2'}, {'name' : '各种业务3'}]},
-		{'name' : '信息录入', 'img' : './static/img/work/信息录入.png', 'show' : false, 'subApp' : [{'name' : '各种业务1'}, {'name' : '各种业务2'}, {'name' : '各种业务3'}]},
-		{'name' : '企业BPM',  'img' : './static/img/work/企业BPM.png', 'show' : false, 'subApp' : [{'name' : '各种业务1'}, {'name' : '各种业务2'}, {'name' : '各种业务3'}]},
-		{'name' : '企业邮箱', 'img' : './static/img/work/企业邮箱.png', 'show' : false, 'subApp' : [{'name' : '各种业务1'}, {'name' : '各种业务2'}, {'name' : '各种业务3'}]},
-		{'name' : '其他',     'img' : './static/img/work/其他.png', 'show' : false, 'subApp' : [{'name' : '各种业务1'}, {'name' : '各种业务2'}, {'name' : '各种业务3'}]}
+		{'name' : '竞争情报系统',   'img' : './static/img/work/销售任务.png', 'show' : false, 'subApp' : [{'name' : '人力共享系统'}, {'name' : '网报系统'}, {'name' : '电子采购平台'}, {'name' : '合同管理系统'}]},
+		{'name' : '档案管理系统',   'img' : './static/img/work/拜访管理.png', 'show' : false, 'subApp' : [{'name' : '人力共享系统'}, {'name' : '网报系统'}, {'name' : '电子采购平台'}, {'name' : '合同管理系统'}]},
+		{'name' : '云学堂',         'img' : './static/img/work/信息协同.png', 'show' : false, 'subApp' : [{'name' : '人力共享系统'}, {'name' : '网报系统'}, {'name' : '电子采购平台'}, {'name' : '合同管理系统'}]},
+		{'name' : '公司官网',       'img' : './static/img/work/数据看板.png', 'show' : false, 'subApp' : [{'name' : '人力共享系统'}, {'name' : '网报系统'}, {'name' : '电子采购平台'}, {'name' : '合同管理系统'}]},
+		{'name' : '决策支持系统',   'img' : './static/img/work/信息录入.png', 'show' : false, 'subApp' : [{'name' : '人力共享系统'}, {'name' : '网报系统'}, {'name' : '电子采购平台'}, {'name' : '合同管理系统'}]},
+		{'name' : 'NC系统',        'img' : './static/img/work/企业BPM.png', 'show' : false, 'subApp' : [{'name' : '人力共享系统'}, {'name' : '网报系统'}, {'name' : '电子采购平台'}, {'name' : '合同管理系统'}]},
+		{'name' : 'BI运营数据平台', 'img' : './static/img/work/企业邮箱.png', 'show' : false, 'subApp' : [{'name' : '人力共享系统'}, {'name' : '网报系统'}, {'name' : '电子采购平台'}, {'name' : '合同管理系统'}]},
+		{'name' : '其他',          'img' : './static/img/work/其他.png', 'show' : false, 'subApp' : [{'name' : '人力共享系统'}, {'name' : '网报系统'}, {'name' : '电子采购平台'}, {'name' : '合同管理系统'}]}
 	]
 };
 
